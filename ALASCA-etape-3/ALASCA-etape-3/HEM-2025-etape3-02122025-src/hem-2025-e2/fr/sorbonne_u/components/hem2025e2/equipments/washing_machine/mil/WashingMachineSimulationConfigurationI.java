d82b4a3be2843baeb850c928d809ce09a2c9a7dc
Moufees95 <moufees@daiki.fr>
2026-02-05 19:02:46 +0100

    delete etape 3

--- ALASCA-etape-3/ALASCA-etape-3/HEM-2025-etape3-02122025-src/hem-2025-e2/fr/sorbonne_u/components/hem2025e2/equipments/washing_machine/mil/WashingMachineSimulationConfigurationI.java
+++
@@ -1,9 +0,0 @@
-package fr.sorbonne_u.components.hem2025e2.equipments.washing_machine.mil;
-
-import java.util.concurrent.TimeUnit;
-
-public interface WashingMachineSimulationConfigurationI 
-{
-    // On définit l'unité de temps de la simulation (Minutes pour une machine à laver, c'est cohérent)
-    public static final TimeUnit TIME_UNIT = TimeUnit.MINUTES;
-}
